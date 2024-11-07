22838401b2edaaa1729efc448e3cb9c9fe315c35
Gustav Toft <gtoft98@gmail.com>
2024-11-07 14:13:35 +0100

    Fix folder structure

--- scripts/loss.py
+++
@@ -1,6 +0,0 @@
-import torch
-from torch.nn.functional import mse_loss as torch_mse_loss
-
-def mse_loss(output: torch.Tensor,
-             target: torch.Tensor) -> torch.Tensor:
-    return torch_mse_loss(output, target)
